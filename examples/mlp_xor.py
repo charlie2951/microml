40c2326c96c881a95349c5df94238b7fc2f175c4
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-09-20 11:15:59 +0530

    Update mlp_xor.py

--- examples/mlp_xor.py
+++ examples/mlp_xor.py
@@ -1,40 +1,21 @@
-import array
+#XOR training demo
 import microml
+from ulab import numpy as np
+import mlp_model as m
+print("MLP XOR Prediction---->")
+X_train=np.array([[0,0],[0,1],[1,0],[1,1]])
+y_train=np.array([0,1,1,0])
+mlp = microml.MLP([2,4,4,2], False)
+mlp.fit(X_train, y_train, 3000, 0.01, 0.9)
+y_pred = []
+for sample in X_train:
+    label = mlp.predict(sample)
+    y_pred.append(label)
+print(y_pred)
+y_p=np.array(y_pred) #convert list to array
+#confusion matrix
+from microml.metrics import confusion_matrix, classification_report 
+confusion_matrix(y_train, y_p,title='XOR Prediction')
+classification_report(y_train,y_p, digits=4)
 
-# 1. Initialize a 3-layer MLP: 2 Inputs -> 4 Hidden Neurons -> 2 Output Classes (0 or 1)
-# Architecture: [2, 4, 2], is_regression=False
-mlp = microml.MLP([2, 4,4,2], False)
 
-# 2. Prepare XOR Training Dataset (4 samples, 2 features each)
-X_data = array.array('f', [
-    0.0, 0.0,  # Sample 0 -> Output 0
-    0.0, 1.0,  # Sample 1 -> Output 1
-    1.0, 0.0,  # Sample 2 -> Output 1
-    1.0, 1.0   # Sample 3 -> Output 0
-])
-
-# Target labels (0 or 1) stored as float array
-y_data = array.array('f', [0.0, 1.0, 1.0, 0.0])
-
-# 3. Train the Model
-# Parameters: fit(X, y, epochs=2000, learning_rate=0.05, momentum=0.9)
-print("Training XOR network...")
-mlp.fit(X_data, y_data, 10000, 0.001, 0.9)
-
-# 4. Evaluate Predictions
-print("\n--- XOR Predictions ---")
-test_inputs = [
-    [0.0, 0.0],
-    [0.0, 1.0],
-    [1.0, 0.0],
-    [1.0, 1.0]
-]
-
-for sample in test_inputs:
-    x_test = array.array('f', sample)
-    pred_class = mlp.predict(x_test)
-    probs = mlp.predict_proba(x_test)
-    
-    print("Input: {} -> Predicted Class: {} (Probabilities: Class 0: {:.2f}%, Class 1: {:.2f}%)".format(
-        sample, pred_class, probs[0] * 100, probs[1] * 100
-    ))
