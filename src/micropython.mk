70c264c11174beda72cbab0d05a0ac6a9173053a
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-09-26 21:51:49 +0530

    Update micropython.mk

--- src/micropython.mk
+++ src/micropython.mk
@@ -3,7 +3,7 @@ USERMOD_DIR := $(USERMOD_DIR)
 # Add C files
 SRC_USERMOD += $(USERMOD_DIR)/microml.c
 SRC_USERMOD += $(USERMOD_DIR)/mlp.c
-#SRC_USERMOD += $(USERMOD_DIR)/decision_tree.c
+SRC_USERMOD += $(USERMOD_DIR)/cnn.c
 #SRC_USERMOD += $(USERMOD_DIR)/svm.c
 
 # Add flags if required
